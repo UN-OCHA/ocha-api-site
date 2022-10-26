2a9e253e0f56b28caa007ad559f0a8c94d4a0010
Peter Droogmans (attiks) <peter@attiks.com>
2022-10-26 10:03:49 +0200

    List users

---
+++ src/Command/UserListCommand.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Utils\Validator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+/**
+ * A console command that list users.
+ */
+#[AsCommand(
+    name: 'app:list-user',
+    description: 'List users'
+)]
+class UserListCommand extends Command
+{
+    private SymfonyStyle $io;
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher,
+        private Validator $validator,
+        private UserRepository $users
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setHelp($this->getCommandHelp())
+        ;
+    }
+
+    /**
+     * This optional method is the first one executed for a command after configure()
+     * and is useful to initialize properties based on the input arguments and options.
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        // SymfonyStyle is an optional feature that Symfony provides so you can
+        // apply a consistent look to the commands of your application.
+        // See https://symfony.com/doc/current/console/style.html
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
+    /**
+     * This method is executed after interact() and initialize(). It usually
+     * contains the logic to execute to complete this command task.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $stopwatch = new Stopwatch();
+        $stopwatch->start('list-user-command');
+
+        $users = $this->users->findAll();
+
+        $table = new Table($output);
+        $table->setHeaders(['Email', 'Roles', 'Token']);
+
+        foreach ($users as $user) {
+            /** @var \App\Entity\User $user */
+            $table->addRow([
+                $user->getEmail(),
+                implode(', ', $user->getRoles()),
+                $user->getToken(),
+            ]);
+        }
+        $table->render();
+
+        $stopwatch->stop('list-user-command');
+        return Command::SUCCESS;
+    }
+
+    /**
+     * The command help is usually included in the configure() method, but when
+     * it's too long, it's better to define a separate method to maintain the
+     * code readability.
+     */
+    private function getCommandHelp(): string
+    {
+        return 'The <info>%command.name%</info> command list users in the database';
+    }
+}
