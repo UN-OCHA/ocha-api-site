e0d3ea32e0cbd22884600c40e97c888f5d89a9f4
Peter Droogmans (attiks) <peter@attiks.com>
2022-11-10 11:36:27 +0100

    chore: clean migrations

--- migrations/Version20221110091933.php
+++ migrations/Version20221110091933.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221108105539 extends AbstractMigration
+final class Version20221110091933 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,13 @@ final class Version20221108105539 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD can_read JSON DEFAULT NULL, ADD can_write JSON DEFAULT NULL');
-        $this->addSql('UPDATE user set can_read = providers');
-        $this->addSql('UPDATE user set can_write = "[]"');
+        $this->addSql('ALTER TABLE key_figures ADD extra JSON DEFAULT "[]"');
+        $this->addSql('UPDATE key_figures SET extra = "[]"');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP can_read, DROP can_write');
+        $this->addSql('ALTER TABLE key_figures DROP extra');
     }
 }
