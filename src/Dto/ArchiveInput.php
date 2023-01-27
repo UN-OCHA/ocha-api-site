cdc42e1f0301fdd12d709c44ca47caec70d4b95c
Peter Droogmans (attiks) <peter@attiks.com>
2023-01-27 11:49:31 +0100

    test: fix

--- src/Dto/ArchiveInput.php
+++ src/Dto/ArchiveInput.php
@@ -3,5 +3,5 @@
 namespace App\Dto;
 
 class ArchiveInput {
-    public function __construct(public string $iso3, public string $year) {}
+    public function __construct(public string $iso3 = "", public string $year = "") {}
 }
